5884d047ed17603d4004029bd4db5e41a41a0f54
Dilenio Enderle <dilenio@gmail.com>
2021-06-05 18:49:09 -0300

    feat: req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    time = 0
+    if start_time == [] or target_time == 0:
+        return 0
+
+    for x1, x2 in zip(start_time, end_time):
+        if target_time >= x1 and target_time <= x2:
+            time += 1
+    return time
