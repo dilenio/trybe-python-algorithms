fbc572548929799a6594101357878a65a50b6f0b
Dilenio Enderle <dilenio@gmail.com>
2021-06-05 18:56:30 -0300

    feat: req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if len(word) == 1:
+        return True
+
+    if word[low] == word[high]:
+        substr = word[1:len(word) - 1]
+        return is_palindrome_recursive(substr, 0, len(substr) - 1)
+
+    return False
